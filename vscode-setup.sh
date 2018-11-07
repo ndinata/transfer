0a477797b35e0043172269f7b82778f50d07a8cd
Nico Dinata <ncdnt@icloud.com>
2018-11-08 10:15:26 +1100

    Install vscode extensions

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -3,4 +3,7 @@
 echo "Installing VS Code..."
 brew cask install visual-studio-code
 
-# Install packages
+# Install extensions
+echo "Installing VS Code extensions..."
+code --install-extension zhuangtongfa.material-theme
+code --install-extension ms-python.python
