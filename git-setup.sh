7080de8ade8b08bb953ad46b04914c9875a0b843
Nico Dinata <ncdnt@icloud.com>
2018-11-08 10:04:48 +1100

    Add some more configurations in .gitconfig

--- git-setup.sh
+++ git-setup.sh
@@ -9,9 +9,18 @@ read full_name
 printf "Type in your Github email address: "
 read email
 
+echo "Setting your full name and email address..."
 git config --global user.name $full_name
 git config --global user.email $email
+
+echo "Setting osxkeychain as credential helper..."
 git config --global credential.helper osxkeychain
+
+echo "Setting the default commit message editor to TextEdit..."
+git config --global core.editor open -W -n
+
+echo "Setting prune to true when fetching..."
+git config --global fetch.prune true
 
 echo ".gitconfig has now been added in $HOME."
 echo "git-setup is done!"
