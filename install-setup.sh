e7b164612f93cf5d57bbd51294577c5c8b2578c3
Nico Dinata <ncdnt@icloud.com>
2018-11-08 09:36:56 +1100

    Install some more apps

--- install-setup.sh
+++ install-setup.sh
@@ -22,6 +22,62 @@ brew cask install franz
 # Install VSCode
 bash vscode-setup.sh
 
+# Install Rocket
+echo "Installing Rocket..."
+brew cask install rocket
+
+# Install Firefox
+echo "Installing Firefox..."
+brew cask install firefox
+
+# Install Cryptomator
+echo "Installing Cryptomator..."
+brew cask install cryptomator
+
+# Install Spotify
+echo "Installing Spotify..."
+brew cask install spotify
+
+# Install Dash
+echo "Installing Dash..."
+brew cask install dash
+
+# Install Discord
+echo "Installing Discord..."
+brew cask install discord
+
+# Install Flock
+echo "Installing Flock..."
+mas install 883594849
+
+# Install 1Password 7
+echo "Installing 1Password..."
+mas install 1333542190
+
+# Install Pages
+echo "Installing Pages..."
+mas install 409201541
+
+# Install Numbers
+echo "Installing Numbers..."
+mas install 409203825
+
+# Install Keynote
+echo "Installing Keynote..."
+mas install 409183694
+
+# Install Magnet
+echo "Installing Magnet..."
+mas install 441258766
+
+# Install Garageband
+echo "Installing Garageband..."
+mas install 682658836
+
+# Install Xcode
+# echo "Installing Xcode..."
+# mas install 497799835
+
 ####################################
 # Install binary commands
 ####################################
