a5b2315ec7b5b5a05144517d1e53713b97e398f2
Nico Dinata <ncdnt@icloud.com>
2018-11-07 23:35:36 +1100

    Separate git-setup

---
+++ git-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Commence git-setup."
+echo "Type in your first and last name: "
+read full_name
+
+echo "Type in your Github email address: "
+read email
+
+git config --global user.name $full_name
+git config --global user.email $email
+git config --global credential.helper osxkeychain
+
+echo "git-setup is done!"
--- setup.sh
+++ setup.sh
@@ -19,18 +19,5 @@ brew upgrade --all
 echo "Installing MAS..."
 brew install mas
 
-#########################################
-# Setup Git
-#########################################
-
-echo "Let's set up git."
-echo "Type in your first and last name: "
-read full_name
-
-echo "Type in your Github email address: "
-read email
-
-git config --global user.name $full_name
-git config --global user.email $email
-
-echo "Git is all set!"
+# Set up git
+bash git-setup.sh
