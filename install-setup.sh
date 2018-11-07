e3f00db744269ab4d8c1919e8a8e73cc0853c827
Nico Dinata <ncdnt@icloud.com>
2018-11-08 00:11:47 +1100

    Update install-setup

--- install-setup.sh
+++ install-setup.sh
@@ -7,5 +7,34 @@ echo "Commence install-setup."
 echo "Installing MAS..."
 brew install mas
 
+####################################
+# Install GUI applications
+####################################
+
+# Install Dropbox
+echo "Installing Dropbox..."
+brew cask install dropbox
+
+# Install Franz
+echo "Installing Franz..."
+brew cask install franz
+
+# Install VSCode
+bash vscode-setup.sh
+
+####################################
+# Install binary commands
+####################################
+
+echo "Installing binary commands..."
+brew install bash
+brew install nano
+brew install wget
+brew install python
+
+
+# Cleanup
+brew update && brew upgrade && brew cleanup && brew doctor
+
 echo "install-setup is done!"
 printf "------------------------\n"
