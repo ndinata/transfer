517f5a71e3eb3b3719506ca70677cd1ed34795f2
Nico Dinata <ncdnt@icloud.com>
2018-11-07 23:29:14 +1100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Commence qsetup."
+
+# Install Xcode command-line tools
+echo "Installing Xcode command-line tools..."
+xcode-select --install
+
+# Install Homebrew
+echo "Installing Homebrew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update and upgrade Homebrew
+echo "Updating Homebrew..."
+brew update
+brew upgrade --all
+
+# Install Mac App Store CLI
+echo "Installing MAS..."
+brew install mas
+
+#########################################
+# Setup Git
+#########################################
+
 echo "Let's set up git."
 echo "Type in your first and last name: "
 read full_name
