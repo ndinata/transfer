c55d277549a559d54565d6950b08c2170318e30d
Nico Dinata <ncdnt@icloud.com>
2018-11-08 12:49:33 +1100

    Update vscode-setup

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -7,3 +7,7 @@ brew cask install visual-studio-code
 echo "Installing VS Code extensions..."
 code --install-extension zhuangtongfa.material-theme
 code --install-extension ms-python.python
+
+# Setup user preferences
+echo "Copying user settings..."
+cp settings.json $HOME/Library/Application\ Support/Code/User/
