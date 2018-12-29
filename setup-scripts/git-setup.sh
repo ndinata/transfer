934a2103a7aa239b437d2857767fff138eac5306
Nico Dinata <ncdnt@icloud.com>
2018-12-30 01:06:31 +1100

    No longer use TextEdit as default commit msg editor

--- setup-scripts/git-setup.sh
+++ setup-scripts/git-setup.sh
@@ -12,9 +12,6 @@ echo "Setting your full name and email address..."
 git config --global user.name "$full_name"
 git config --global user.email $email
 
-echo "Setting the default commit message editor to TextEdit..."
-git config --global core.editor "open -W -n"
-
 echo "Setting prune to true when fetching..."
 git config --global fetch.prune true
 
