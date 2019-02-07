918610a592a97e282e3925393da47cfe2b04aa7f
Nico Dinata <ncdnt@icloud.com>
2019-02-07 20:55:16 +1100

    Fix installing pure prompt without fish

--- setup-scripts/fish-setup.sh
+++ setup-scripts/fish-setup.sh
@@ -10,16 +10,16 @@ sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
 echo && echo "Changing default shell to fish..."
 chsh -s /usr/local/bin/fish
 
-echo && echo "Downloading and installing fish-pure..."
+echo && echo "Downloading fish-pure..."
 curl git.io/pure-fish --output /tmp/pure_installer.fish --location --silent
-source /tmp/pure_installer.fish; and install_pure
+echo "Remember to actually install the prompt by firing a fish session and typing this:"
+echo "source /tmp/pure_installer.fish; and install_pure"
 
-echo "Remember to add Brew's Python symlink location to PATH!"
+echo && echo "Remember to add Brew's Python symlink location to PATH!"
 echo "Fire up a fish session and type this:"
-echo && echo "set -U fish_user_paths /usr/local/opt/python/libexec/bin \$fish_user_paths"
-echo
+echo "set -U fish_user_paths /usr/local/opt/python/libexec/bin \$fish_user_paths"
 
-echo "Also, enable vi keybindings on fish by typing this:"
+echo && echo "Also, enable vi keybindings on fish by typing this:"
 echo && echo "fish_vi_key_bindings"
 echo
 
