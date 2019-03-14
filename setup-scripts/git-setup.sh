111391cea1f8fc7bea97848ac5c3c2b36b46a04a
Nico Dinata <ncdnt@icloud.com>
2019-03-14 23:44:24 +1100

    Add git log alias

--- setup-scripts/git-setup.sh
+++ setup-scripts/git-setup.sh
@@ -15,5 +15,9 @@ git config --global user.email $email
 echo "Setting prune to true when fetching..."
 git config --global fetch.prune true
 
+echo "Setting lg as log alias..."
+git config --global alias.lg "log --graph --all --format=format:'%C(blue)%h%C(reset)  %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(yellow)%d%C(reset)%n''         %C(dim cyan)%aD%C(reset) %C(dim green)%ar%C(reset)'"
+
 echo ".gitconfig has now been added in $HOME."
 echo "end git-setup.sh"
+
