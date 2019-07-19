019449452252cb2c064e5fda58aa6e959ee93398
Nico Dinata <ncdnt@icloud.com>
2019-07-19 22:49:49 +1000

    No longer need interaction for git setup

--- setup-scripts/git-setup.sh
+++ setup-scripts/git-setup.sh
@@ -5,15 +5,5 @@ echo && echo "git-setup.sh"
 echo "Copying .gitconfig..."
 cp dotfiles/.gitconfig $HOME/
 
-printf "Enter your full name: "
-read full_name
-
-printf "Enter your Github email address: "
-read email
-
-echo "Setting your full name and email address..."
-git config --global user.name "$full_name"
-git config --global user.email $email
-
 echo "end git-setup.sh"
 
