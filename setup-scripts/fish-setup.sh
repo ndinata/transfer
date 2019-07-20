9e55a28126379a0c6ea32a081b6f10a05f6cac2a
Nico Dinata <ncdnt@icloud.com>
2019-07-20 22:43:43 +1000

    Install fish pure

--- setup-scripts/fish-setup.sh
+++ setup-scripts/fish-setup.sh
@@ -13,6 +13,9 @@ cp fishfiles/* $HOME/.config/fish/functions
 echo "Installing Fisher..."
 curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
 
+echo "Installing fish pure..."
+fish -c "fisher add rafaelrinaldi/pure"
+
 # Add the new bash to the list of allowed shells
 echo "Adding fish to the list of allowed shells..."
 sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
@@ -21,20 +24,20 @@ sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
 echo && echo "Changing default shell to fish..."
 chsh -s /usr/local/bin/fish
 
-echo && echo "Downloading fish-pure..."
-curl git.io/pure-fish --output /tmp/pure_installer.fish --location --silent
+# echo && echo "Downloading fish-pure..."
+# curl git.io/pure-fish --output /tmp/pure_installer.fish --location --silent
 
 echo && echo "Creating fish directory for linking..."
 mkdir -pv $HOME/.config/fish/conf.d/
 
-echo && echo "Remember to do 3 things:"
-echo "1. Install the prompt by starting a fish session and typing this:"
-echo "source /tmp/pure_installer.fish; and install_pure"
+echo && echo "Remember to do 2 things:"
+# echo "1. Install the prompt by starting a fish session and typing this:"
+# echo "source /tmp/pure_installer.fish; and install_pure"
 
-echo && echo "2. Add Brew's Python symlink location to PATH by starting a fish session and typing this:"
+echo && echo "1. Add Brew's Python symlink location to PATH by starting a fish session and typing this:"
 echo "set -U fish_user_paths /usr/local/opt/python/libexec/bin \$fish_user_paths"
 
-echo && echo "3. Enable vi keybindings on fish by typing this:"
+echo && echo "2. Enable vi keybindings on fish by typing this:"
 echo "fish_vi_key_bindings"
 echo
 
