cc41a4f2de8268fedafdf76d2127f355bc325a08
Nico Dinata <ncdnt@icloud.com>
2020-04-04 18:07:52 +1100

    Update vim-plug installation dir

--- helpers/vim.sh
+++ helpers/vim.sh
@@ -3,16 +3,18 @@
 vimrc_dir="dotfiles/.vimrc"
 vim_home_dir="$HOME/.vim"
 vimrc_home_dir="$vim_home_dir/vimrc"
-vim_plug_install_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+vim_plug_download_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+vim_plug_install_dir="~/.local/share/nvim/site/autoload/plug.vim"
+
 vimrc_cmd() {
     mkdir -pv "$vim_home_dir"
     cp "$vimrc_dir" "$vimrc_home_dir"
 }
 vim_plug_install_cmd() {
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs "$vim_plug_install_url"
+    curl -fLo "$vim_plug_install_dir" --create-dirs "$vim_plug_download_url"
     if [ $? -ne 0 ]; then
         echo -e "\n"
-        errcho "$ERROR something went wrong when downloading vim-plug file"
+        errcho "$ERROR something went wrong when downloading vim-plug"
         errcho "Please check the generated \`$VIM_LOGFILE\`."
     fi
 }
