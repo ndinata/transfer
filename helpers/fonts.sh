eecf889d0d54568a28768ba0614707b8a102b000
Nico Dinata <nicodnt@icloud.com>
2020-09-28 14:36:33 +1000

    [font] Fix filename change in Big Sur
    Closes #19

--- helpers/fonts.sh
+++ helpers/fonts.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 sf_source_dir="/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
-sf_source_dir_catalina="/System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
+sf_source_dir_big_sur="/System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
 sf_test_file="SFMono-Regular.otf"
+sf_test_file_big_sur="SF-Mono-Regular.otf"
 sf_target_dir="$HOME/Library/Fonts"
 
 ###########################################################################
@@ -18,22 +19,28 @@ fi
 
 # Install SFMono font, if available
 are_fontfiles_available=false
+is_big_sur=false
 
 echo -n "Looking for SFMono font files..."
 if [ -f $sf_source_dir/$sf_test_file ]; then
     are_fontfiles_available=true
-elif [ -f $sf_source_dir_catalina/$sf_test_file ]; then
+elif [ -f $sf_source_dir_big_sur/$sf_test_file_big_sur ]; then
     are_fontfiles_available=true
-    sf_source_dir="$sf_source_dir_catalina"
+    sf_source_dir="$sf_source_dir_big_sur"
+    is_big_sur=true
 fi
 
 if [[ "$are_fontfiles_available" == true ]]; then
     echo -e "\rLooking for SFMono font files... They're available!"
     echo -n "Installing them..."
-    cp $sf_source_dir/SFMono*.otf $sf_target_dir
+
+    if [[ "$is_big_sur" == true ]]; then
+      cp $sf_source_dir/SF-Mono*.otf $sf_target_dir
+    else
+      cp $sf_source_dir/SFMono*.otf $sf_target_dir
 
     echo "The following font files have been installed in $sf_target_dir:" >> "$FONT_LOGFILE"
-    for fontfile in $sf_target_dir/SFMono*.otf; do
+    for fontfile in $sf_target_dir/*.otf; do
         echo "        $fontfile" >> "$FONT_LOGFILE"
     done
     echo -e "\rInstalling them... Done! $SUCCESS"
