356f1755d1a3112cdaa2043afaa83febb2e54ca4
Nico Dinata <nicodnt@icloud.com>
2020-11-14 09:42:48 +1100

    [iTerm] Remove theme

--- helpers/util.sh
+++ helpers/util.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-iterm_theme_dir="others/OneSnazzy.itermcolors"
-iterm_theme_dl_dir="$HOME/Downloads/"
 tldr_autocomplete_url="https://raw.githubusercontent.com/tldr-pages/tldr-cpp-client/master/autocomplete/complete.fish"
 tldr_autocomplete_path="$HOME/.config/fish/completions/tldr.fish"
 bat_config_content="# Set theme to \"TwoDark\"\n--theme=\"TwoDark\""
 bat_config_path="$HOME/.config/bat"
-iterm_theme_cmd() { cp "$iterm_theme_dir" "$iterm_theme_dl_dir"; }
+
 download_tldr_cmd() { curl "$tldr_autocomplete_url" -sLo "$tldr_autocomplete_path"; }
 setup_config_bat_cmd() {
   mkdir -p "$bat_config_path"
@@ -17,9 +15,6 @@ setup_config_bat_cmd() {
 
 echo_header "Utilities"
 echo "$DIVIDER"
-
-# Copy iTerm theme 
-try_action "Copying iTerm theme file to $iterm_theme_dl_dir" iterm_theme_cmd
 
 # Download autocomplete file for `tldr`
 try_action "Downloading tldr autocomplete file" download_tldr_cmd
