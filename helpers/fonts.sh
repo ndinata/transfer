95ecdaaa63170fa1594ac1bd56a8192ca6be9776
Nico Dinata <nicodnt@icloud.com>
2022-12-30 23:17:15 +0700

    Remove outdated font location path

--- helpers/fonts.sh
+++ helpers/fonts.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-sf_source_dir="/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
-sf_source_dir_big_sur="/System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
-sf_test_file="SFMono-Regular.otf"
-sf_test_file_big_sur="SF-Mono-Regular.otf"
+sf_source_dir="/System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts"
+sf_test_file="SF-Mono-Regular.otf"
 sf_target_dir="$HOME/Library/Fonts"
 
 ###########################################################################
@@ -19,32 +17,23 @@ fi
 
 # Install SFMono font, if available
 are_fontfiles_available=false
-is_big_sur=false
 
 echo -n "Looking for SFMono font files..."
 if [ -f $sf_source_dir/$sf_test_file ]; then
     are_fontfiles_available=true
-elif [ -f $sf_source_dir_big_sur/$sf_test_file_big_sur ]; then
-    are_fontfiles_available=true
-    sf_source_dir="$sf_source_dir_big_sur"
-    is_big_sur=true
 fi
 
 if [[ "$are_fontfiles_available" == true ]]; then
     echo -e "\rLooking for SFMono font files... They're available!"
-    echo -n "Installing them..."
+    echo -n "Copying them..."
 
-    if [[ "$is_big_sur" == true ]]; then
-      cp $sf_source_dir/SF-Mono*.otf $sf_target_dir
-    else
-      cp $sf_source_dir/SFMono*.otf $sf_target_dir
-    fi
+    cp $sf_source_dir/SF-Mono*.otf $sf_target_dir
 
-    echo "The following font files have been installed in $sf_target_dir:" >> "$FONT_LOGFILE"
+    echo "The following font files have been copied to $sf_target_dir:" >> "$FONT_LOGFILE"
     for fontfile in $sf_target_dir/*.otf; do
         echo "        $fontfile" >> "$FONT_LOGFILE"
     done
-    echo -e "\rInstalling them... Done! $SUCCESS"
+    echo -e "\rCopying them... Done! $SUCCESS"
 else
     echo -e "\rLooking for SFMono font files... $FAIL"
     errcho "$ERROR SFMono font files seem to be missing, or their extension is not .otf"
