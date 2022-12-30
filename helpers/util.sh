67872e1a01bc3e2ade3c6e20a9805c949a9a2be6
Nico Dinata <nicodnt@icloud.com>
2022-12-31 00:05:30 +0700

    Update utils

--- helpers/util.sh
+++ helpers/util.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 fish_config_file="$HOME/.config/fish/config.fish"
-tldr_autocomplete_url="https://raw.githubusercontent.com/tldr-pages/tldr-cpp-client/master/autocomplete/complete.fish"
-tldr_autocomplete_path="$HOME/.config/fish/completions/tldr.fish"
+# tldr_autocomplete_url="https://raw.githubusercontent.com/tldr-pages/tldr-cpp-client/master/autocomplete/complete.fish"
+# tldr_autocomplete_path="$HOME/.config/fish/completions/tldr.fish"
 bat_config_content="# Set theme to \"TwoDark\"\n--theme=\"TwoDark\""
 bat_config_path="$HOME/.config/bat"
 local_rg_config_file="dotfiles/.ripgreprc"
 rg_config_path="$HOME/.config/ripgrep/"
 rg_config_file_name=".ripgreprc"
 
-download_tldr_cmd() { curl "$tldr_autocomplete_url" -sLo "$tldr_autocomplete_path"; }
+# download_tldr_cmd() { curl "$tldr_autocomplete_url" -sLo "$tldr_autocomplete_path"; }
 setup_config_bat_cmd() {
   mkdir -p "$bat_config_path"
   echo -e "$bat_config_content" >> "$bat_config_path/config"
@@ -17,7 +17,7 @@ setup_config_bat_cmd() {
 setup_rg_config_file_cmd() {
   mkdir -p "$rg_config_path"
   cp "$local_rg_config_file" "$rg_config_path"
-  echo -e "\nset -g RIPGREP_CONFIG_PATH $rg_config_path$rg_config_file_name" >> "$fish_config_file"
+  fish -c "set -Ux RIPGREP_CONFIG_PATH /Users/nico/.config/ripgrep/ripgreprc"
 }
 
 ###########################################################################
@@ -26,7 +26,7 @@ echo_header "Utilities"
 echo "$DIVIDER"
 
 # Download autocomplete file for `tldr`
-try_action "Downloading tldr autocomplete file" download_tldr_cmd
+# try_action "Downloading tldr autocomplete file" download_tldr_cmd
 
 # Setting up config file for `bat`
 try_action "Setting up config file for bat" setup_config_bat_cmd
