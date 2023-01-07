42fe14e2b6954187bc5f46e048ed62fa7fe47f71
Nico Dinata <nicodnt@icloud.com>
2023-01-07 23:42:43 +0700

    Replace Brew node with fnm

--- helpers/fish.sh
+++ helpers/fish.sh
@@ -18,9 +18,15 @@ fish_pkg_install_cmd() {
     # fish -c "fisher install jorgebucaran/nvm.fish"
     # fish -c "fisher install wfxr/forgit"
 }
-fish_python_node_cmd() {
+fish_python_fnm_cmd() {
     fish -c "fish_add_path /opt/homebrew/opt/python@3.10/libexec/bin"
-    fish -c "fish_add_path /opt/homebrew/opt/node@16/bin"
+
+    fnm_fish_path="$HOME/.config/fish/conf.d"
+    if [ ! -d "$fnm_fish_path/" ]; then
+        mkdir -p $fnm_fish_path
+    fi
+    touch $fnm_fish_path/fnm.fish
+    echo "fnm env --use-on-cd | source" >> $fnm_fish_path/fnm.fish
 }
 # fish_forgit_cmd() {
 #   echo "set -x FORGIT_FZF_DEFAULT_OPTS \"\$FORGIT_FZF_DEFAULT_OPTS --reverse --cycle\"" >> "$fish_config_file"
@@ -41,8 +47,8 @@ try_action "Installing Fisher" fisher_install_cmd "$FISH_LOGFILE"
 # Install fish packages
 try_action "Installing fish packages" fish_pkg_install_cmd "$FISH_LOGFILE"
 
-# Setup fish Python & Node PATH
-try_action "Adding Brew's python and node symlink location to \$PATH" fish_python_node_cmd
+# Setup fish Python & fnm
+try_action "Complete setup of Brew's python and fnm" fish_python_fnm_cmd
 
 # Set up custom options for forgit
 # try_action "Setting up custom options for forgit" fish_forgit_cmd "$FISH_LOGFILE"
