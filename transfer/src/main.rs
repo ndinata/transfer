79d23131050008839c142c06a1b0478cd50ff93c
Nico Dinata <nicodnt@icloud.com>
2023-04-22 23:23:40 +0700

    Log more info on main program

--- transfer/src/main.rs
+++ transfer/src/main.rs
@@ -7,35 +7,44 @@ async fn main() {
 
     const CONFIG_FILE_PATH: &str = "config/config.toml";
 
+    eprintln!("Parsing config file at {}.", CONFIG_FILE_PATH);
     let config = match transfer_config::parse_config(CONFIG_FILE_PATH) {
         Ok(c) => c,
         Err(e) => {
-            println!("ConfigError('{CONFIG_FILE_PATH}'): {e}");
+            eprintln!("ConfigError('{CONFIG_FILE_PATH}'): {e}");
             process::exit(1);
         }
     };
 
+    eprintln!("Config file parsed successfully.");
+    eprintln!();
+
+    eprintln!("Copying files...");
     for to_copy in config.copy {
         if let Err(e) = transfer_lib::copy_file(&to_copy.from, &to_copy.to) {
-            println!(
+            eprintln!(
                 "CopyFileError('{0}' -> '{1}'): {e}",
                 to_copy.from, to_copy.to
             );
         }
     }
+    eprintln!("Done copying files.");
+    eprintln!();
 
+    eprintln!("Starting file downloads...");
     let downloader = Downloader::new();
     for to_download in config.download {
         let log_download_progress = |(downloaded, total_size)| match total_size {
             Some(total) => {
                 let percentage = downloaded * 100 / total;
-                println!("Downloaded {}% ({}/{}B)", percentage, downloaded, total);
+                eprintln!("Downloaded {}% ({}/{}B)", percentage, downloaded, total);
             }
             None => {
-                println!("Downloaded {}B", downloaded);
+                eprintln!("Downloaded {}B", downloaded);
             }
         };
 
+        eprintln!("Downloading {}:", to_download.from);
         if to_download.to.is_some() {
             if let Err(e) = downloader
                 .download(
@@ -45,27 +54,33 @@ async fn main() {
                 )
                 .await
             {
-                println!("DownloadError('{0}'): {e}", to_download.from);
+                eprintln!("DownloadError('{0}'): {e}", to_download.from);
             }
         } else if to_download.run.is_some() {
             if let Err(e) = downloader
                 .download_and_source(&to_download.from, log_download_progress)
                 .await
             {
-                println!("DownloadError('{0}'): {e}", to_download.from);
+                eprintln!("DownloadError('{0}'): {e}", to_download.from);
             }
         }
     }
+    eprintln!("Done downloading files.");
+    eprintln!();
 
+    eprintln!("Running scripts...");
     for to_run in config.run {
+        eprintln!("{}...", to_run.title);
         if let Err(e) = transfer_lib::run_script(
             &to_run.command,
             vec![&to_run.script_path],
             &to_run.script_path,
         ) {
-            println!("RunScriptError('{0}'): {e}", to_run.script_path);
+            eprintln!("RunScriptError('{0}'): {e}", to_run.script_path);
         }
     }
+    eprintln!("Done running scripts.");
+    eprintln!();
 
     println!("Don't forget to also do these things!");
     for (i, to_remind) in config.reminders.iter().enumerate() {
