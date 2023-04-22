cb7fcc41da701370f6300ccebfeb7acf063146cd
Nico Dinata <nicodnt@icloud.com>
2023-04-22 22:30:15 +0700

    Complete setting up pyenv

--- config/scripts/setup.fish
+++ config/scripts/setup.fish
@@ -1,5 +1,5 @@
 # Set ripgrep config file path
-set -Ux RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+set -Ux RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
 
 # Install fisher
 fisher install jorgebucaran/fisher
@@ -10,3 +10,4 @@ fisher install pure-fish/pure
 # Set up pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
+echo "pyenv init - | source" >> $HOME/.config/fish/config.fish
