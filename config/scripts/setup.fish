359710746f52d113e26c4c4fb7e7a7a669224750
Nico Dinata <nicodnt@icloud.com>
2023-04-22 22:38:20 +0700

    Complete setting up rbenv

--- config/scripts/setup.fish
+++ config/scripts/setup.fish
@@ -1,13 +1,16 @@
-# Set ripgrep config file path
-set -Ux RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
-
 # Install fisher
 fisher install jorgebucaran/fisher
 
 # Install pure
 fisher install pure-fish/pure
 
+# Set ripgrep config file path
+set -Ux RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
+
 # Set up pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
 echo "pyenv init - | source" >> $HOME/.config/fish/config.fish
+
+# Set up rbenv
+echo "status --is-interactive; and rbenv init - fish | source" >> $HOME/.config/fish/config.fish
