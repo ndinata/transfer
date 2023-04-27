b7d529f91ac1f9bcb0dbff809ccac623174d8d9f
Nico Dinata <nicodnt@icloud.com>
2023-04-27 21:29:52 +0700

    Expose lib error types

--- transfer-lib/src/lib.rs
+++ transfer-lib/src/lib.rs
@@ -5,9 +5,9 @@ mod download;
 mod remind;
 mod run;
 
-pub use self::brew::Brew;
+pub use self::brew::{Brew, BrewError};
 pub use self::config::parse_config;
-pub use self::copy::Copyable;
-pub use self::download::Downloadable;
+pub use self::copy::{CopyError, Copyable};
+pub use self::download::{DownloadError, Downloadable};
 pub use self::remind::Remindable;
-pub use self::run::Runnable;
+pub use self::run::{RunError, Runnable};
