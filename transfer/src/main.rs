1142161568fd3054d3535b75df44af27f5dc4ea0
Nico Dinata <nicodnt@icloud.com>
2023-04-27 14:44:41 +0700

    Update main to use new lib interface

--- transfer/src/main.rs
+++ transfer/src/main.rs
@@ -1,5 +1,4 @@
 use std::process;
-use transfer_lib::{self, Brew, Downloader};
 
 fn main() {
     println!("Hello, world!");
@@ -7,7 +6,7 @@ fn main() {
     const CONFIG_FILE_PATH: &str = "config/config.toml";
 
     eprintln!("Parsing config file at `{}`...", CONFIG_FILE_PATH);
-    let config = match transfer_config::parse_config(CONFIG_FILE_PATH) {
+    let config = match transfer_lib::parse_config(CONFIG_FILE_PATH) {
         Ok(c) => c,
         Err(e) => {
             eprintln!("ConfigError('{CONFIG_FILE_PATH}'): {e}, exiting.");
@@ -18,15 +17,10 @@ fn main() {
     eprintln!();
 
     eprintln!("Installing Homebrew...");
-    let brew = match Brew::init().and_then(|brew| {
-        brew.install_self()?;
-        Ok(brew)
-    }) {
-        Ok(brew) => brew,
-        Err(e) => {
-            eprintln!("BrewError: {e}, exiting.");
-            process::exit(1);
-        }
+    let brew = config.brew;
+    if let Err(e) = brew.install_self() {
+        eprintln!("BrewError: {e}, exiting.");
+        process::exit(1);
     };
     eprintln!("Done installing Homebrew.");
     eprintln!();
@@ -37,14 +31,14 @@ fn main() {
 
     eprintln!("Installing Brew taps...");
     if let Err(e) = brew
-        .install_taps(config.brew.taps, brew_install_progress_cb)
+        .install_taps(brew_install_progress_cb)
         .and_then(|_| {
             eprintln!("Installing Brew formulae...");
-            brew.install_formulae(config.brew.formulae, brew_install_progress_cb)
+            brew.install_formulae(brew_install_progress_cb)
         })
         .and_then(|_| {
             eprintln!("Installing Brew casks...");
-            brew.install_casks(config.brew.casks, brew_install_progress_cb)
+            brew.install_casks(brew_install_progress_cb)
         })
     {
         eprintln!("BrewError: {e}, exiting.");
@@ -53,67 +47,42 @@ fn main() {
 
     eprintln!();
     eprintln!("Copying files...");
-    for to_copy in config.copy {
-        if let Err(e) = transfer_lib::copy_file(&to_copy.from, &to_copy.to) {
+    for copyable in config.copy {
+        if let Err(e) = copyable.copy() {
             eprintln!(
                 "CopyFileError('{0}' -> '{1}'): {e}",
-                to_copy.from, to_copy.to
+                copyable.from, copyable.to
             );
         }
     }
     eprintln!("Done copying files.");
     eprintln!();
 
-    let downloader = Downloader::init();
-    if let Err(e) = downloader {
-        eprintln!("DownloaderError: {e}, skipping all downloads.",);
-    } else {
-        eprintln!("Starting file downloads...");
-        let mut downloader = downloader.unwrap();
-        for to_download in config.download {
-            let log_download_progress = |(downloaded, total)| {
-                let percentage = downloaded * 100 / total;
-                eprintln!("Downloaded {}% ({}/{}B)", percentage, downloaded, total);
-            };
-
-            if to_download.to.is_some() {
-                if let Err(e) = downloader.download(
-                    &to_download.from,
-                    &to_download.to.unwrap(),
-                    log_download_progress,
-                ) {
-                    eprintln!("DownloadError('{0}'): {e}", to_download.from);
-                }
-            } else if to_download.run.is_some() {
-                if let Err(e) =
-                    downloader.download_and_source(&to_download.from, log_download_progress)
-                {
-                    eprintln!("DownloadError('{0}'): {e}", to_download.from);
-                }
-            }
+    eprintln!("Starting file downloads...");
+    for mut downloadable in config.download {
+        if let Err(e) = downloadable.download(|(downloaded, total)| {
+            let percentage = downloaded * 100 / total;
+            eprintln!("Downloaded {}% ({}/{}B)", percentage, downloaded, total);
+        }) {
+            eprintln!("DownloadError('{0}'): {e}", downloadable.from);
         }
-        eprintln!("Done downloading files.");
     }
+    eprintln!("Done downloading files.");
     eprintln!();
 
     eprintln!("Running scripts...");
-    for to_run in config.run {
-        eprintln!("{}...", to_run.title);
-        if let Err(e) = transfer_lib::run_script(
-            &to_run.command,
-            vec![&to_run.script_path],
-            &to_run.script_path,
-        ) {
-            eprintln!("RunScriptError('{0}'): {e}", to_run.script_path);
+    for runnable in config.run {
+        eprintln!("{}...", runnable.title);
+        if let Err(e) = runnable.run_script() {
+            eprintln!("RunScriptError('{0}'): {e}", runnable.script_path);
         }
     }
     eprintln!("Done running scripts.");
     eprintln!();
 
     println!("Don't forget to also do these things!");
-    for (i, to_remind) in config.reminders.iter().enumerate() {
-        println!("{}. {}", i + 1, to_remind.instruction);
-        println!("$ {}", to_remind.command);
+    for (i, remindable) in config.reminders.iter().enumerate() {
+        remindable.display_reminder(i + 1);
         println!();
     }
 
