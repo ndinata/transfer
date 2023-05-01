2fbe28311dee94d8c55bee8da396d37101c3e860
Nico Dinata <nicodnt@icloud.com>
2023-05-01 15:08:13 +0700

    Add comments for xcode funcs

--- transfer/src/xcode.rs
+++ transfer/src/xcode.rs
@@ -1,13 +1,15 @@
 use std::process::Command;
 
+/// Whether Xcode command-line tools have been installed on this machine.
 pub fn require_xcode_tools() -> bool {
     let c = Command::new("xcode-select").arg("-p").output();
     c.is_err() || !c.unwrap().status.success()
 }
 
+/// Prints instructions to install Xcode command-line tools to stderr.
 pub fn show_xcode_instructions() {
-    println!("It seems like you haven't installed Xcode's command-line tools.");
-    println!("You can do so by running this command:");
-    println!("  xcode-select --install");
-    println!("Please try again after you've done this!");
+    eprintln!("It seems like you haven't installed Xcode's command-line tools.");
+    eprintln!("You can do so by running this command:");
+    eprintln!("  xcode-select --install");
+    eprintln!("Please try again after you've done this!");
 }
