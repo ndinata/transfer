df3f76cd516d278b43415a47df3354aac097d093
Nico Dinata <nicodnt@icloud.com>
2023-05-05 22:27:46 +0700

    Add updated Brew function impls

---
+++ transfer/src/config/brew.rs
@@ -0,0 +1,84 @@
+use std::path::Path;
+use std::process::Command;
+
+use anyhow::{ensure, Result};
+
+pub fn install_homebrew() -> Result<()> {
+    const HOMEBREW_INSTALL_URL: &str =
+        "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh";
+
+    let output = Command::new("curl")
+        .arg("-fsSL")
+        .arg(HOMEBREW_INSTALL_URL)
+        .env("NONINTERACTIVE", "1")
+        .output()?;
+    ensure!(
+        output.status.success(),
+        "something went wrong when installing Homebrew"
+    );
+
+    let output = Command::new("eval")
+        .arg("$(/opt/homebrew/bin/brew shellenv)")
+        .output()?;
+    ensure!(
+        output.status.success(),
+        "unable to expose Homebrew bin directory"
+    );
+
+    Ok(())
+}
+
+pub fn update_brew() -> Result<()> {
+    let mut brew = Command::new("brew");
+
+    let output = brew.arg("update").output()?;
+    ensure!(
+        output.status.success(),
+        "`brew update` failed to terminate successfully"
+    );
+    let output = brew.arg("upgrade").output()?;
+    ensure!(
+        output.status.success(),
+        "`brew upgrade` failed to terminate successfully"
+    );
+    Ok(())
+}
+
+pub fn disable_brew_analytics() -> Result<()> {
+    let output = Command::new("brew").arg("analytics").arg("off").output()?;
+    ensure!(
+        output.status.success(),
+        "`brew analytics off` failed to terminate successfully"
+    );
+    Ok(())
+}
+
+pub fn install_bundle<P: AsRef<Path>>(brewfile_path: P) -> Result<()> {
+    let brewfile_path_str = brewfile_path.as_ref().to_str();
+    ensure!(
+        brewfile_path_str.is_some(),
+        "cannot find valid Brewfile at '{}'",
+        brewfile_path.as_ref().display()
+    );
+
+    let output = Command::new("brew")
+        .arg("bundle")
+        .env("HOMEBREW_BUNDLE_FILE", brewfile_path_str.unwrap())
+        .output()?;
+
+    ensure!(
+        output.status.success(),
+        "`brew bundle` failed to terminate successfully"
+    );
+    Ok(())
+}
+
+#[test]
+fn bash_works() {
+    let c = Command::new("bash")
+        .arg("-c")
+        .arg("echo 'hello world!'")
+        .status()
+        .unwrap();
+    assert!(c.success());
+}
--- transfer/src/config/mod.rs
+++ transfer/src/config/mod.rs
@@ -1,3 +1,4 @@
+mod brew;
 mod copy;
 mod download;
 mod remind;
