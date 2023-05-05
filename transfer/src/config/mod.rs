20be0e234e88cd6b5db4b5319a1ae0fcc4e19d80
Nico Dinata <nicodnt@icloud.com>
2023-05-05 21:01:50 +0700

    Add config parsing to main pkg

--- transfer/src/config/mod.rs
+++ transfer/src/config/mod.rs
@@ -2,3 +2,38 @@ mod copy;
 mod download;
 mod remind;
 mod run;
+
+use std::fs;
+use std::path::Path;
+
+use anyhow::Result;
+use serde::Deserialize;
+
+/// Parses the TOML file at `file_path` and serialises it into the schema
+/// defined in `Config`.
+///
+/// # Errors
+/// This function returns an error if:
+/// - the file at `file_path` cannot be read into a `String`, or
+/// - the file doesn't match the schema defined in `Config`
+pub fn parse_config<P: AsRef<Path>>(file_path: P) -> Result<Config> {
+    let content = fs::read_to_string(file_path)?;
+    let config: Config = toml::from_str(&content)?;
+    Ok(config)
+}
+
+/// Configuration schema for adjusting the behaviour of the program.
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    /// Files to copy.
+    pub copy: Vec<copy::Copyable>,
+
+    /// Files to download.
+    pub download: Vec<download::Downloadable>,
+
+    /// Scripts to run.
+    pub run: Vec<run::Runnable>,
+
+    /// Reminders to be displayed.
+    pub reminders: Vec<remind::Remindable>,
+}
--- transfer/src/dir.rs
+++ transfer/src/dir.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use anyhow::anyhow;
+use anyhow::{anyhow, Result};
 use simple_home_dir::expand_tilde;
 
 /// Creates the dir if it doesn't exist yet, together with all its parent components.
@@ -10,7 +10,7 @@ use simple_home_dir::expand_tilde;
 /// This function may return an error if the $HOME envvar isn't set to a valid value.
 ///
 /// This function also returns an error if creating the dir (and its parents) fails.
-pub fn create_dir<P: AsRef<Path>>(dir: P) -> anyhow::Result<PathBuf> {
+pub fn create_dir<P: AsRef<Path>>(dir: P) -> Result<PathBuf> {
     let dir = expand_tilde(dir).ok_or_else(|| anyhow!("$HOME is not set or invalid"))?;
     if !dir.exists() {
         fs::create_dir_all(&dir)?;
